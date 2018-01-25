3eb2e8ccc060e5174e6c8e6f45bb4135293c838a
mushmelow <zhenghao22@gmail.com>
2018-01-26 07:44:59 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//handle dependencies
+
+const express= require('express');
+const morgan= require('morgan');
+const bodyParser= require('body-parser');
+
+const app = express();
+
+//Middlewares
+
+//routes
+
+//start the server
+const port = process.env.PORT || 3000;
